46115f46ea68ef724ec2563ac28ac96bc8eea121
Gurox <93458769+GuroxDG@users.noreply.github.com>
2023-08-10 01:28:20 -0500

    first insert

--- CreateOrGetData.py
+++ CreateOrGetData.py
@@ -1,4 +1,4 @@
-
+import sys
 from useful_functions import ReaderConfig
 from useful_functions import GetData
 from sqlalchemy import create_engine, Double, String, Table, MetaData, Date, Column
@@ -29,11 +29,11 @@ timeSeries = Table(
     'Time_Series_FX', meta, 
     Column('from_symbol',String(5), primary_key=True), 
     Column('to_symbol',  String(5), primary_key=True), 
-    Column('date',  Date), 
-    Column('1_open', Double), 
-    Column('2_high', Double), 
-    Column('3_low',  Double), 
-    Column('4_close',Double), 
+    Column('date',  Date, primary_key=True), 
+    Column('1. open', Double), 
+    Column('2. high', Double), 
+    Column('3. low',  Double), 
+    Column('4. close',Double), 
 )
 
 meta.create_all(engine)
@@ -41,6 +41,16 @@ connection.commit()
 
 api = config.get_api()
 data = GetData(api_=api)
-data_json = data.get_json('EUR','USD')
+
+from_symbol = 'EUR'
+to_symbol = 'USD'
+data_json = data.get_json(from_symbol,to_symbol)
 tipos_data = list(data_json.keys())
-print(len(data_json[tipos_data[1]]))
+
+data_insert = data_json[tipos_data[1]]
+data_insert = [data_insert[i]|{'date':i, 'from_symbol': from_symbol, 'to_symbol':to_symbol} for i in data_insert]
+
+connection.execute(timeSeries.insert(),
+    data_insert                   
+)
+connection.commit()
