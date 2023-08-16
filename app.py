597b8565013d8887ca1d4325d806a9f2c020d92e
Gurox <93458769+GuroxDG@users.noreply.github.com>
2023-08-16 01:10:49 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes.timeseries_routes import user
+import uvicorn
 
 app = FastAPI(
     title="Users API",
@@ -7,3 +8,6 @@ app = FastAPI(
     version="0.0.1",
 )
 app.include_router(user)
+
+# if __name__ == '__main__':
+#     uvicorn.run('app:app', host='127.0.0.1 ', port=8000, reload=True)
